3f48c51e1df0f0f6132db4e02952875304f5230a
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2022-04-22 18:41:46 -0500

    add humidity value

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,17 +1,19 @@
 const request = require('request');
 
-const forecast = (latitude, longitude , callback) => {
+const forecast = (latitude, longitude, callback) => {
     const key = '4580190775d5eb2d92486394927975e4';
     const url = `http://api.weatherstack.com/current?access_key=${key}&query=${latitude}, ${longitude}`;
 
-    request({ url, json: true }, (error, { body }={}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
-            callback( 'Unable to connect to weather service!', undefined );
+            callback('Unable to connect to weather service!', undefined);
         }
-        else if (body.success ===  false) {
+        else if (body.success === false) {
             callback(body.error.info, undefined);
         } else {
-            const resp = `${body.current.weather_descriptions[0]}. It's currently ${body.current.temperature} degress out. There is a  ${body.current.precip}% change of rain `
+            const resp = `${body.current.weather_descriptions[0]}. 
+                            It's currently ${body.current.temperature} degress out. There is a  
+                            ${body.current.precip}% change of rain and humidity value for ${body.current.humidity}%`
             callback(undefined, resp)
         }
     })
