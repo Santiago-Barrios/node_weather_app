9ac60292cbe6381d6bcd741a59614eb1585ef234
Santiago Barrios <santiagobarriosgrijalba.08@gmail.com>
2022-04-22 13:18:58 -0500

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -8,8 +8,8 @@ weatherForm.addEventListener('submit', (event) => {
     const location = search.value;
     messageOne.textContent = 'Loading ...';
     messageTwo.textContent = '';
-    
-    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+
+    fetch(`/weather?address=${location}`).then((response) => {
         return response.json().then((data) => {
             if (data.error) {
                 return messageOne.textContent = data.error ;
